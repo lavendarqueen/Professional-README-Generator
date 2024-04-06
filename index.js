accfd508aa05d4406192e4d15497d9fb982a1a88
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-04-05 22:24:32 -0600

    Added inquirer and fs declarations, prompts, and license functions to index.js and generateMarkdown function to project.

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
 // TODO: Include packages needed for this application
-
+const inquirer =  require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+{
+    type: "input",
+    name: "projectTitle",
+    message: "Please enter the title of your project.",
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please enter a brief description of your web page.",
+    licenseName: ["license",]
+},
+{
+    type: "input",
+    name: "installInstructions",
+    message: "Please enter installation instructions.",
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Please describe intended usage information.",
+},
+{
+    type: "input",
+    name: "contributions",
+    message: "Please list all credits, contributors, and collaboration on your project",
+},
+{
+    type: "input",
+    name: "contributionGuidelines",
+    message: "Please describe contribution guidelines for your project."
+},
+{
+    type: "input",
+    name: "repo",
+    message: "Please describe test instructions."    
+},
+{
+    type: "list",
+    name: "license",
+    message: "Please explain the type of license that covers your project.",
+    chooseLicense: ["mit", "apache","unilicense"], 
+},
+{   
+    type: "input",
+    name: "dependencies",
+    message: "Please enter npm i to install dependencies."
+},
+{
+    type: "input",
+    name: "gitHubUserName",
+    message: "Please enter your GitHub Username.",
+},
+{   
+    type: "input",
+    name: "email",
+    message: "Please enter your email address.",
+},
+])
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "mit") {
+    return "![Static Badge](https://Image.shields.io/badge/License-MIT_?style=flat&color-purple)";
+  } else if (license === "apache") {
+    return "![Static Badge](https://Image.shields.io/badge/License-Apache-_?style=flat&color=Blue)";
+  } else if (license === "the unilicense") {
+    return "![Static Badge](https://Image.shields.io/badge/License-the_unilicense-_?style=flat&color=green)";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
