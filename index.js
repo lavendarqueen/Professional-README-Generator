41313aab8645bf1b082abb2786aa3ba5efa42301
PhyllisAnnsComputer <plataille6121@gmail.com>
2024-04-07 18:06:52 -0600

    Added function to generate Table of Contents

--- index.js
+++ index.js
@@ -1,74 +1,105 @@
 // TODO: Include packages needed for this application
-const inquirer =  require("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+
+const api = require(".utils/api.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // TODO: Create an array of questions for user input
-inquirer
-.prompt([
-{
+inquirer.prompt([
+  {
     type: "input",
     name: "projectTitle",
     message: "Please enter the title of your project.",
-},
-{
+  },
+  {
     type: "input",
     name: "description",
     message: "Please enter a brief description of your web page.",
-    licenseName: ["license",]
-},
-{
+    licenseName: ["license"],
+  },
+  {
     type: "input",
-    name: "installInstructions",
+    name: "installation",
     message: "Please enter installation instructions.",
-},
-{
+  },
+  {
     type: "input",
     name: "usage",
     message: "Please describe intended usage information.",
-},
-{
-    type: "input",
-    name: "contributions",
-    message: "Please list all credits, contributors, and collaboration on your project",
-},
-{
-    type: "input",
-    name: "contributionGuidelines",
-    message: "Please describe contribution guidelines for your project."
-},
-{
-    type: "input",
-    name: "repo",
-    message: "Please describe test instructions."    
-},
-{
+  },
+  {
     type: "list",
     name: "license",
     message: "Please explain the type of license that covers your project.",
-    chooseLicense: ["mit", "apache","unilicense"], 
-},
-{   
+    chooseLicense: ["mit", "apache", "unilicense"],
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "Please describe contributing guidelines for your project.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please describe test instructions.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "Please list all credits, contributions, and collaboration on your project",
+  },
+  {
     type: "input",
     name: "dependencies",
-    message: "Please enter npm i to install dependencies."
-},
-{
+    message: "Please enter npm i to install dependencies.",
+  },
+
+  {
     type: "input",
     name: "gitHubUserName",
     message: "Please enter your GitHub Username.",
-},
-{   
+  },
+  {
     type: "input",
     name: "email",
     message: "Please enter your email address.",
-},
-])
-
+  },
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(README, data) {}
+function writeToFile(README, data) {
+  fs.writeFile(README, Data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success! Your README.md file has been generated");
+  });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  try {
+    const userResponses = await inquirer.prompt(questions);
+    console.log("Your responses: ", userResponses);
+    console.log("Thank you for your responses! Fetching your data...");
+
+    const userInfo = await api.getUser(userResponses);
+    console.log("Your Github user info: ", userInfo);
+
+    console.log("Generating your README...");
+    const markdown = generateMarkdown(userResponses, userInfo);
+    console.log(markdown);
+
+    await writeFileAsync("ExampleREADME.md", markdown);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,22 +7,101 @@ function renderLicenseBadge(license) {
     return "![Static Badge](https://Image.shields.io/badge/License-Apache-_?style=flat&color=Blue)";
   } else if (license === "the unilicense") {
     return "![Static Badge](https://Image.shields.io/badge/License-the_unilicense-_?style=flat&color=green)";
+  } else if (license === "null") {
+    return "";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "null") {
+    return console.log("A license is required. Please select a license.");
+  } else {
+    return "!(//https://github.com/![userName, projectName]/blob/main/LICENSE)";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseSection(license) {
+  if (license === "null") {
+  } else {
+    generateMarkdown(licenseSection);
+  }
 }
 
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(userResponses, userInfo) {
+  let myMarkdown = `# ${userResponses.title}`;
+  let myContents = `## Table of Contents`;
+  let questions = `## Questions`;
+}
+
+// CREATE TABLE OF CONTENTS
+// # Table of contents
+// [Description](#Description)
+// [Installation](#Installation)
+// [Usage](#Usage)
+// [License](#License)
+// [ContributingGuidelines](#Contributing)
+// [Tests](#Tests)
+// [Credits](#Credits)
+// [Questions](#Questions)
+// [GitHub](#GitHub)
+// [Contact_Me](#ContactMe)
+{
+  if (userResponses.title !== "") return `# ${title}`;
+}
+
+{
+  if (userResponses.description !== "") {
+    description += `* [Description](#description)`;
+  }
+}
+
+{
+  if (userResponses.installation !== "") {
+    installation += `* [Installation Instructions](#installation)`;
+  }
+}
+
+{
+  if (userResponses.usage !== "") {
+    usage += `* [Usage Information](#usage)`;
+  }
+}
+
+{
+  if (userResponses.contribution !== "") {
+    contribution += `* [Contribution Guidelines](#contribution)`;
+  }
+}
+
+{
+  if (userResponses.tests !== "") {
+    tests += `* [Test Instructions](#tests)`;
+  }
+}
+
+{
+  if (userResponses.credits !== "") {
+    credits += `* [Credits](#credits)`;
+  }
+}
+
+{
+  if (userResponses.questions !== "") {
+    questions += `* [Questions](#questions)`;
+  }
+}
+
+//GENERATE MARKDOWN FOR EACH SECTION OF THE README FILE
+
 module.exports = generateMarkdown;
+
+// ## GitHub
+// ${data.userName}
+
+// ## Contact Me
+// "Please contact me at ${data.email} for additional information."
